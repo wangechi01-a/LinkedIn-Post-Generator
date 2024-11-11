df2023407d474ab902ec3645e8856c16ceda60a5
wangechi01-@ <wangechieunice0@gmail.com>
2024-11-11 16:32:26 +0300

    project

---
+++ preprocess.py
@@ -0,0 +1,86 @@
+import json
+from llm_helper import llm
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+
+
+def process_posts(raw_file_path, processed_file_path=None):
+    with open(raw_file_path, encoding='utf-8') as file:
+        posts = json.load(file)
+        enriched_posts = []
+        for post in posts:
+            metadata = extract_metadata(post['text'])
+            post_with_metadata = post | metadata
+            enriched_posts.append(post_with_metadata)
+
+    unified_tags = get_unified_tags(enriched_posts)
+    for post in enriched_posts:
+        current_tags = post['tags']
+        new_tags = {unified_tags[tag] for tag in current_tags}
+        post['tags'] = list(new_tags)
+
+    with open(processed_file_path, encoding='utf-8', mode="w") as outfile:
+        json.dump(enriched_posts, outfile, indent=4)
+
+
+def extract_metadata(post):
+    template = '''
+    You are given a LinkedIn post. You need to extract number of lines, language of the post and tags.
+    1. Return a valid JSON. No preamble. 
+    2. JSON object should have exactly three keys: line_count, language and tags. 
+    3. tags is an array of text tags. Extract maximum two tags.
+    4. Language should be English or Swahili
+    
+    Here is the actual post on which you need to perform this task:  
+    {post}
+    '''
+
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={"post": post})
+
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
+    return res
+
+
+def get_unified_tags(posts_with_metadata):
+    unique_tags = set()
+    # Loop through each post and extract the tags
+    for post in posts_with_metadata:
+        unique_tags.update(post['tags'])  # Add the tags to the set
+
+    unique_tags_list = ','.join(unique_tags)
+
+    template = '''I will give you a list of tags. You need to unify tags with the following requirements,
+    1. Tags are unified and merged to create a shorter list. 
+       Example 1: "AI", "Artificial Intelligence", "Machine Learning", "Natural Language Processing", "Computer Vision" can all be merged into a single tag "AI".
+       Example 2: "Innovation", "Innovative", "Cutting-edge", "Breakthrough", "Reimagining" can be mapped to "Innovation".
+       Example 3: "Resilience", "Toughness", "Perseverance", "Adaptability", "Grit", "Mental Strength", "Overcoming Setbacks" can be mapped to "Resilience".
+       Example 4: "Career Growth", "Professional Development", "Skill Development", "Career Advancement", "Job Progression", "Professional Growth", "Career Development" can be mapped to "Career Growth".
+       Example 5: "Motivation", "Inspiration", "Drive", "Determination", "Ambition", "Passion", "Encouragement", "Focus", "Commitment" can be mapped to "Motivation".
+    2. Each tag should be follow title case convention. example: "Motivation", "Innovation"
+    3. Output should be a JSON object, No preamble
+    3. Output should have mapping of original tag and the unified tag. 
+       For example: {{"Career Growth": "Professional Development",  "Resilience": "Adaptability", "Innovation": "Innovative"}}
+    
+    Here is the list of tags: 
+    {tags}
+    '''
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={"tags": str(unique_tags_list)})
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
+    return res
+
+
+if __name__ == "__main__":
+    process_posts("data/raw_posts.json", "data/processed_posts.json")
